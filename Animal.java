d7d18ed5158f5f5c29c0d5d6af5d137a9894923b
Jean <jean.debois@realdolmen.com>
2017-10-09 11:09:43 +0200

    fianl Animal.class

--- Animal.java
+++ Animal.java
@@ -4,14 +4,9 @@
 public class Animal{
 	private String name;
 	private double height;
-
-<<<<<<< HEAD
-
-	public Animal() {}
-=======
+	
 	public Animal(String name, double height) {
 		this.name = name;
 		this.height = height;
 	}
->>>>>>> karabudjan
 }
